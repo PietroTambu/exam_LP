265cd72a998fcdd04c341ad197b11bebae1e959a
Pietro Tamburini <pietro.tamburini.3@gmail.com>
2024-01-06 16:01:23 +0100

    feat: gitignore old ool

--- Lisp/ool.lisp
+++ Lisp/ool.lisp
@@ -36,15 +36,18 @@
 ;;; Checks the type compatibility of each field in a list of fields.
 ;;; Raises an error if a field's value does not match its declared type.
 (defun check-fields-types (fields)
-  (dolist (field fields)
-    (let ((field-name (first field))
-          (field-value (second field))
-          (field-type (third field)))
-      (when (and (> (length field) 2)
-                 (not (or (class-spec field-type)
-                          (ignore-errors (typep field-value field-type)))))
-        (error "Error: field value ~A (~A) doesn't match type (~A)"
-               field-name field-value field-type)))))
+  (let ((errors (mapcan (lambda (field)
+                          (let ((field-name (first field))
+                                (field-value (second field))
+                                (field-type (third field)))
+                            (when (and (> (length field) 2)
+                                       (not (or (class-spec field-type)
+                                                (ignore-errors (typep field-value field-type)))))
+                              (list (format nil "Error: field value ~A (~A) doesn't match type (~A)"
+                                            field-name field-value field-type)))))
+                        fields)))
+    (unless (null errors)
+      (error (format nil "~{~A~^, ~}" errors)))))
 
 ;;; Retrieves the attributes of a defined class
 (defun get-attributes (class-name)
@@ -63,36 +66,45 @@
     (when class-specification
       (let ((parents (second class-specification)))
         (nconc (copy-list parents)
-               (loop for parent in parents
-                     nconc (get-parents parent)))))))
+               (mapcan #'get-parents parents))))))
 
 ;;; Generates a sorted list of unique parent classes for a list of classes, 
 ;;; removing any duplicates.
 (defun get-parents-list-sorted (classes-name)
-  (remove-duplicates (loop for class-name in classes-name
-                           nconc (get-parents class-name))
+  (remove-duplicates (mapcan #'get-parents classes-name)
                      :from-end t))
 
 ;;; Ensures each field in a list has three elements: name, value, and a type. 
 ;;; Adds default values and types if none are provided.
 (defun ensure-field-types (fields)
-  (loop for field in fields
-        collect (cond ((= (length field) 1) (append field '(NIL T)))
-                      ((= (length field) 2) (append field '(T)))
-                      (t field))))
+  (mapcar (lambda (field)
+            (cond ((= (length field) 1) (append field '(NIL T)))
+                  ((= (length field) 2) (append field '(T)))
+                  (t field)))
+          fields))
+
+;;; The pair-up function transforms a flat list into a list of pairs.
+;;; It recursively pairs every two consecutive elements of the input list.
+(defun pair-up (lst)
+  (if (null lst)
+      nil
+      (cons (list (car lst) (cadr lst))
+            (pair-up (cddr lst)))))  
 
 ;;; Validates that field types in a class are subtypes of the same fields in
 ;;; its parent classes, raising an error for any type mismatches.
 (defun check-subtype-integrity (parents fields)
-  (loop for parent in (append parents (get-parents-list-sorted parents))
-    do (loop for attribute in (get-attributes parent)
-      do (let ((matching-attribute 
-                (find (first attribute) fields :key #'first)))
-           (unless (or (not matching-attribute)
-                       (subtypep (third matching-attribute)
-                                 (third attribute)))
-             (error "Error: type of slot ~S is a supertype of 
-                     inherited type" (first attribute)))))))
+  (mapc (lambda (parent)
+          (mapc (lambda (attribute)
+                  (let ((matching-attribute 
+                         (find (first attribute) fields :key #'first)))
+                    (unless (or (not matching-attribute)
+                                (subtypep (third matching-attribute)
+                                          (third attribute)))
+                      (error "Error: type of slot ~S is a supertype of 
+                              inherited type" (first attribute)))))
+                (get-attributes parent)))
+        (append parents (get-parents-list-sorted parents))))
 
 ;;; Links a method to its implementation based on class and method names,
 ;;; handling method invocation or signaling an error if the method is not found.
@@ -110,34 +122,33 @@
 ;;; Collects attributes from parent classes not present in 'parts', using a 
 ;;; specified function to retrieve attributes from each parent.
 (defun get-missing-parts (get-parts-function parents parts)
-  (loop for parent in 
-        (nconc parents 
-                (get-parents-list-sorted parents))
-        nconc (loop for attribute in 
-                    (funcall get-parts-function parent)
-                    unless (find (first attribute)
-                                  parts 
-                                  :key #'first)
-                    collect attribute)))
+  (mapcan (lambda (parent)
+            (mapcan (lambda (attribute)
+                      (unless (find (first attribute) parts :key #'first)
+                        (list attribute)))
+                    (funcall get-parts-function parent)))
+          (nconc parents (get-parents-list-sorted parents))))
 
 ;;; (def-class <class-name> <parents> <part>*) => <class-name | error>
 ;;;
 ;;; Defines a new class with fields and methods, including inherited ones,
 ;;; and performs type checks and integrity validations.
 (defun def-class (class-name parents &rest parts)
-  (let* ((fields (loop for part in parts
-                       when (eq (first part) 'fields)
-                       nconc (rest part)))
-         (methods (loop for part in parts
-                        when (eq (first part) 'methods)
-                        nconc (rest part)))
+  (let* ((fields (mapcan (lambda (part)
+                           (when (eq (first part) 'fields)
+                             (rest part)))
+                         parts))
+         (methods (mapcan (lambda (part)
+                            (when (eq (first part) 'methods)
+                              (rest part)))
+                          parts))
          (final-fields (ensure-field-types
                         (append fields
                                 (get-missing-parts 'get-attributes parents
                                                    fields))))
          (final-methods (append methods
                                 (get-missing-parts 'get-methods parents
-                                                    methods))))
+                                                   methods))))
 
     (check-subtype-integrity parents fields)
     (check-fields-types fields)
@@ -156,41 +167,44 @@
   (let* ( (classes-chain (append (get-parents-list-sorted (list class-name))
                                 (list class-name)))
           (class-fields (get-attributes class-name))
-          (fields (loop for (key value) on parts by #'cddr
-                      collect (list key value)))
+          (fields (pair-up parts))
           (missing-fields (remove-duplicates
-                            (loop for class in classes-chain nconc
-                                  (loop for attribute in (get-attributes class)
-                                        unless (find (first attribute) fields
-                                                      :key #'first)
-                                        collect (list (first attribute)
-                                                      (second attribute))))
+                            (mapcan (lambda (class)
+                              (mapcan (lambda (attribute)
+                                (unless (find (first attribute)
+                                              fields :key #'first)
+                                        (list (list (first attribute)
+                                                    (second attribute)))))
+                                      (get-attributes class)))
+                              classes-chain)
                             :key #'first :test #'equal)))
         
-        (loop for field in fields
-              do (let ((class-field (find (first field) class-fields
-                                          :key #'first)))
-                    (unless class-field
-                      (error "Error: Illegal attribute ~A found"
-                              (first field)))
-                    (unless (typep (second field) (third class-field))
-                      (error "Error: value ~S for field ~S is not of type ~S."
-                              (second field) (first class-field)
-                              (third class-field)))))
+        (mapc (lambda (field)
+                (let ((class-field (find (first field)
+                                          class-fields :key #'first)))
+                  (unless class-field
+                    (error "Error: Illegal attribute ~A found" (first field)))
+                  (unless (typep (second field) (third class-field))
+                    (error "Error: value ~S for field ~S is not of type ~S."
+                          (second field) (first class-field)
+                          (third class-field)))))
+              fields)
 
-        (loop for missing-field in missing-fields
-              unless (second missing-field)
-              do (error "Error: You must provide a value for NIL default
-                        field ~A." missing-field))
+        (mapc (lambda (missing-field)
+                (unless (second missing-field)
+                  (error "Error: You must provide a value for field ~A." 
+                        missing-field)))
+              missing-fields)
 
-        (loop for method in (get-methods class-name)
-              do (let* ((method-name (first method))
-                        (method-params (cons 'this (second method)))
-                        (method-body (third method)))
-                    (define-method class-name method-name
-                      (eval `(lambda ,method-params ,method-body)))
-                    (method-linker method-name)))
-        
+        (mapc (lambda (method)
+                (let* ((method-name (first method))
+                      (method-params (cons 'this (second method)))
+                      (method-body (third method)))
+                  (define-method class-name method-name
+                    (eval `(lambda ,method-params ,method-body)))
+                  (method-linker method-name)))
+              (get-methods class-name))
+
         (add-instance (list class-name (append fields missing-fields)
                             (get-methods class-name)))))
 
@@ -208,9 +222,11 @@
        (or (eq class-names 'T)
            (let ((parents (get-parents (first (get-instance instance)))))
              (if (listp class-names)
-                 (loop for class-name in class-names
-                       always (member class-name parents :test #'eql))
+                 (every (lambda (class-name) 
+                          (member class-name parents :test #'eql))
+                        class-names)
                  (member class-names parents :test #'eql))))))
+
 
 ;;; (field <instance> <field-name>) => <field-value | error>
 ;;;
@@ -272,9 +288,9 @@
 ; (defparameter p (make 'person 'name "person name"))
 ; (defparameter sb (make 'studente_bicocca 'name "studente bicocca name"))
 
-; (defparameter obj1 (make 'person 'age 4))
+; (defparameter obj1 (make 'person 'name 'obj2))
 
-; (defparameter obj2 (make 'person 'age 5))
+; (defparameter obj2 (make 'person 'name 'obj3))
 
 ; (defparameter obj3 (make 'person 'age 99))
 
